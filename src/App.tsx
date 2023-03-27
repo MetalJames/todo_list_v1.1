5a0da6d636e744c09f7a1ba7f21f613ef8e24013
Volodymyr <volodyaruzhak@gmail.com>
2023-03-27 10:20:19 -0400

    init commite, basic set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      Hello World, its me, Volodymyr
+    </div>
+  )
+}
+
+export default App
